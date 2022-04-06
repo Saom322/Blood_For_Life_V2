4d1aec02199b729cfa34a1935f46b77ebb71d1a3
Saom322 <you@example.com>
2022-04-06 14:55:16 +0600

    Clickable Elements Added in Dashboard

--- app/src/main/java/com/example/bfl/Dashboard.java
+++ app/src/main/java/com/example/bfl/Dashboard.java
@@ -1,15 +1,50 @@
 package com.example.bfl;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Dashboard extends AppCompatActivity {
+    CardView cardSearch;
+    CardView cardLogout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        cardSearch=findViewById(R.id.cardSearch);
+        cardSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard.this, Search.class));
+            }
+        });
+
+        cardLogout=findViewById(R.id.Logout);
+        cardLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard.this, MainActivity.class));
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
